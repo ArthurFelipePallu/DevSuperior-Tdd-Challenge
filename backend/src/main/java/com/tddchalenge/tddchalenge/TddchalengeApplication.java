65d0542f1d4d536d6375c599a06d6b333e9813a4
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-06-19 21:22:18 -0300

    initial maven project

---
+++ backend/src/main/java/com/tddchalenge/tddchalenge/TddchalengeApplication.java
@@ -0,0 +1,13 @@
+package com.tddchalenge.tddchalenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddchalengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddchalengeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tddchalenge/tddchalenge/TddchalengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tddchalenge.tddchalenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddchalengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
